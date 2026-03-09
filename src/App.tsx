09cfcbebae7878e8e84ffab85ce0081e80874001
Meegu80 <aceman80@nate.com>
2026-03-09 18:45:12 +0900

    Squashed 'lecture09-movie/' content from commit 8e8d2f4
    git-subtree-dir: lecture09-movie
    git-subtree-split: 8e8d2f4436d0c5d6cc6a99acc805f19b96390dbd

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import {BrowserRouter, Route, Routes} from "react-router";
+import Detail from "./pages/Detail.tsx";
+import Home from "./pages/Home.tsx";
+import {Reset} from "styled-reset";
+import Search from "./pages/Search.tsx";
+import GlobalStyle from "./styles.ts";
+
+function App() {
+
+    return (
+        <>
+            <GlobalStyle/>
+            <BrowserRouter>
+                <Reset/>
+                <Routes>
+                    <Route path={"/detail/:id"} element={<Detail/>}/>
+                    <Route path={"/search"} element={<Search/>}/>
+                    <Route path={"/"} element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
